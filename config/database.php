b348da08f09aab99dd857788a1e4512e048839d2
Ķeerþhī ßweeþý <selvamvishnu25@gmail.com>
2022-10-08 08:18:08 +0530

    updates env

--- config/database.php
+++ config/database.php
@@ -52,12 +52,16 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            'timezone' => env('DB_TIMEZONE', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'dump' => [
+                'dump_binary_path' => env('DB_DUMP_PATH'),
+            ],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
